ac7ccd71a2feefd10e2cd392a2730c2919a9aa06
SaatoruGojo <harshanand23561@gmail.com>
2023-12-08 12:06:25 +0530

    Updated the result page

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template,request
 import requests
 
-#url = "https://api.openweathermap.org/data/2.5/weather"
-#apikey = "0f0432f6b23fa71d90a64e6bdb432a32"
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,7 +18,7 @@ def getWeather():
     }
     response = requests.get(url,params=param)
     data = response.json()
-    return f"data : {data}"
+    return render_template("weather_result.html", data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
